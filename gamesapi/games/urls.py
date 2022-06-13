912d77946804ed14e29ccc910d49a1a48c548e15
ryenon <ryenon@gmail.com>
2022-06-13 16:43:08 +0000

    create views and route

--- gamesapi/games/urls.py
+++ gamesapi/games/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from django.urls import path
-from games import views
+from django.urls import path, re_path
+#from games import views
 from . import views
 
 urlpatterns = [
-    path(r'^games/$', views.game_list),
-    path(r'^games/(?P<pk>[0-9]+)/$', views.game_detail),
+    re_path('game_list/', views.game_list),
+    re_path('game_detail/(?P<pk>[0-9]+)/', views.game_detail),
 ]
